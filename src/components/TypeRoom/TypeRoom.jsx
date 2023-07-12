a1cade5649336b1506942e55fbd89b5dce378002
Juan Martín López Frau <103914139+juanmaxx2@users.noreply.github.com>
2023-07-12 14:00:00 -0300

    Update TypeRoom.jsx

--- src/components/TypeRoom/TypeRoom.jsx
+++ src/components/TypeRoom/TypeRoom.jsx
@@ -17,7 +17,7 @@ import { v4 as uuidv4 } from "uuid";
 
 //?----------------- COMPONENTE ROOM TYPE  ------------------------------------
 const TypeRoom = ({ id, Trolleys }) => {
-  const URL_BASE = "https://las-casitas-del-hornero-back-deploy.up.railway.app";
+  const URL_BASE = "https://casitasdelhornero.onrender.com";
   const User = PedirLocalStorage();
   const check = PedirCheckInCheckOut();
   const dispatch = useDispatch();
